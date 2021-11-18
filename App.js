86bd261cd0be63aa2ff3314c5f01cf7d6eb762a8
Maksim El'kin <91630929+LiveParadox96@users.noreply.github.com>
2021-11-18 23:15:11 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TodoList from './Todo/TodoList';
+
+
+function App() {
+  const todos =[
+    {id:1, completed: false, title: 'Купить хлеб'},
+    {id:2, completed: false, title: 'Купить масло'},
+    {id:3, completed: false, title: 'Купить молоко'},
+  ]
+  
+  function toggleTodo(id) {
+    console.log ('todo id', id)
+  }
+
+  return (
+    <div className='wrapper'>
+      <h1>React tutorial</h1>
+      <TodoList todos={todos} onToggle={toggleTodo}/>
+    </div>
+  );
+}
+
+export default App;
