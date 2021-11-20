c7c30c1487a6896400abeebe68f212dcf4a13abb
Maksim El'kin <91630929+LiveParadox96@users.noreply.github.com>
2021-11-20 23:53:06 +0300

    Add files via upload

--- education_youtube/src/Todo/TodoItem.js
+++ education_youtube/src/Todo/TodoItem.js
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
 
-function TodoItem({todo, index}) {
+const styles= {
+    li:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5 rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input:{
+        marginRight: '1 rem'
+    }
+}
+
+function TodoItem({ todo, index, onChange }) {
+    const {removeTodo} =useContext(Context)
+    const classes = []
+
+    if (todo.completed){
+        classes.push('done')
+    }
     return(
-        <li>
-        <span>
-            <input type="checkbox"/>
-        <strong>{index +1}</strong>
+        <li style= {styles.li}>
+        <span className= {classes.join('')}>
+            <input
+            type="checkbox"
+            checked={todo.completed}
+            style={styles.input}
+            onChange={()=> onChange(todo.id)}
+            />
+            <strong>{index +1}</strong>
+            &nbsp;
         {todo.title}
         </span>
-        <button>&times;</button>
+
+        <button className='rm' onClick={removeTodo.bind( todo.id )}>
+        &times;
+        </button>
         </li>
     )
 }
 
 TodoItem.propTypes={
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem
